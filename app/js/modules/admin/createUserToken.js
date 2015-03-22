d22e2d2084b01dbf95635d1c14b52bbbedde9f7e
sergey <spectrs@bk.ru>
2015-03-22 22:41:07 +0300

    fix

--- app/js/modules/admin/createUserToken.js
+++ app/js/modules/admin/createUserToken.js
@@ -10,11 +10,14 @@ module.exports = function($scope, $state, tokens) {
     $scope.email = '';
     $scope.firms = $scope.$parent.firms;
     $scope.selectedFirm = $scope.firms.length ? $scope.firms[0] : '';
+    $scope.progress = false;
 
 
     $scope.createToken = function(email, selectedFirm) {
 
         if (!email) return alert('Укажите email');
+
+        $scope.progress = true;
 
         api.createToken(email, selectedFirm._id || '')
             .then(function() {
@@ -22,6 +25,7 @@ module.exports = function($scope, $state, tokens) {
             })
             .catch(function(err) {
                 console.error(err);
+                $scope.progress = false;
 
                 if (err.responseJSON == 'the specified user is already exists')
                     return alert('Ошибка. Пользователь существует');
