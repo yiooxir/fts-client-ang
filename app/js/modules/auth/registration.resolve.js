0950297a79b685763f8d31f6face1ab57bedfe73
sergey <spectrs@bk.ru>
2015-03-27 01:53:39 +0300

    fix

--- app/js/modules/auth/registration.resolve.js
+++ app/js/modules/auth/registration.resolve.js
@@ -5,12 +5,14 @@
 var api = require('../../services/api');
 
 module.exports = {
-    token: function ($stateParams) {
+    token: function ($stateParams, $state) {
         if (!$stateParams.token) return undefined;
 
         return api.getToken($stateParams.token)
             .catch(function (err) {
                 console.error(err);
+                alert('Приглашение не действительно.');
+                $state.go('login');
             })
     }
 };
